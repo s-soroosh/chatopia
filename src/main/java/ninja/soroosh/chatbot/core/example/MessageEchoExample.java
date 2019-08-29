6303c3d3acca3161f7fe3fda9e1999b724aca0ec
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2019-08-29 17:54:17 +0200

    use session in example

--- src/main/java/ninja/soroosh/chatbot/core/example/MessageEchoExample.java
+++ src/main/java/ninja/soroosh/chatbot/core/example/MessageEchoExample.java
@@ -4,7 +4,10 @@ import ninja.soroosh.chatbot.core.annotation.ChatController;
 import ninja.soroosh.chatbot.core.annotation.OnCommand;
 import ninja.soroosh.chatbot.core.runner.Context;
 import ninja.soroosh.chatbot.core.runner.Response;
+import ninja.soroosh.chatbot.core.session.Session;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @ChatController
@@ -16,6 +19,15 @@ public class MessageEchoExample {
 
     @OnCommand("hi")
     public Response onHiCommand(String message, Context context) {
-        return () -> "Hi man, How are you? you session Id is " + context.getSessionId()+ " and you are on channel: "+ context.getChannel();
+        final Session session = context.getSession().get();
+        final Optional<String> previousCount = session.get("count");
+        final String currentCount = previousCount.map(strCount -> String.valueOf(Integer.parseInt(strCount) + 1))
+                .orElse("1");
+        session.set("count", currentCount);
+
+        return () -> "Hi man, How are you? you session Id is "
+                + context.getSessionId() +
+                " and you are on channel: " + context.getChannel() +
+                " and you call me " + currentCount + " times";
     }
 }
