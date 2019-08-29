85c4317f714ee0706508a549f1be4bf5cc546380
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2019-08-29 14:35:43 +0200

    spring boot main method

---
+++ src/main/java/ninja/soroosh/chatbot/core/ChatbotCoreApplication.java
@@ -0,0 +1,11 @@
+package ninja.soroosh.chatbot.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotCoreApplication.class, args);
+    }
+}
