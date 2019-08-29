7ed6a4aa95ca532ab4205c5c4ed644f425768183
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2019-08-29 14:35:21 +0200

    add Pair class

---
+++ src/main/java/ninja/soroosh/chatbot/core/util/Pair.java
@@ -0,0 +1,15 @@
+package ninja.soroosh.chatbot.core.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Pair<T, K> {
+    T item1;
+    K item2;
+
+    public static <T, K> Pair<T, K> of(T item1, K item2) {
+        return new Pair<>(item1, item2);
+    }
+}
