048325adc0894171ca7e35e1909e10cafad534b5
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2019-08-29 17:23:48 +0200

    use context in example

--- src/main/java/ninja/soroosh/chatbot/core/example/MessageEchoExample.java
+++ src/main/java/ninja/soroosh/chatbot/core/example/MessageEchoExample.java
@@ -2,6 +2,7 @@ package ninja.soroosh.chatbot.core.example;
 
 import ninja.soroosh.chatbot.core.annotation.ChatController;
 import ninja.soroosh.chatbot.core.annotation.OnCommand;
+import ninja.soroosh.chatbot.core.runner.Context;
 import ninja.soroosh.chatbot.core.runner.Response;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,12 @@ import org.springframework.stereotype.Service;
 @ChatController
 public class MessageEchoExample {
     @OnCommand("echo")
-    public Response onEchoCommand(String message) {
+    public Response onEchoCommand(String message, Context context) {
         return () -> "echo " + message;
     }
 
     @OnCommand("hi")
-    public Response onHiCommand(String message) {
-        return () -> "Hi man, How are you?";
+    public Response onHiCommand(String message, Context context) {
+        return () -> "Hi man, How are you? you session Id is " + context.getSessionId()+ " and you are on channel: "+ context.getChannel();
     }
 }
