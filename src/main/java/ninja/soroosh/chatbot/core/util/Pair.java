98105f30323b312e3f2b23e0e6523f085850557d
Soroosh Sarabadani <soroosh.sarabadani@gmail.com>
2019-09-09 22:49:02 +0200

    Update Pair.java

--- src/main/java/ninja/soroosh/chatbot/core/util/Pair.java
+++ src/main/java/ninja/soroosh/chatbot/core/util/Pair.java
@@ -6,8 +6,8 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Pair<T, K> {
-    T item1;
-    K item2;
+    private T item1;
+    private K item2;
 
     public static <T, K> Pair<T, K> of(T item1, K item2) {
         return new Pair<>(item1, item2);
