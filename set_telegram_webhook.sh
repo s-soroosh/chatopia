0193253fe3f629372f321e925d4a4f07cd8598c9
Soroosh Sarabadani <soroosh.sarabadani@gmail.com>
2019-10-05 18:00:54 +0200

    document usage

--- set_telegram_webhook.sh
+++ set_telegram_webhook.sh
@@ -1,25 +1,31 @@
-set -x 
+<<Document
+	Examples:
+		- ./set_telegram_webhook.sh 8080 /connectors/telegram TELEGRAM_API_KEY
+                - TELEGRAM_API_KEY=YOUR_TELEGRAM_KEY  ./set_telegram_webhook.sh 9090 / 
+Document
+
 port=$1
 
-if [ -z $2 ]; then
+endpoint=$2
+
+if [ -z $3 ]; then
   key=$TELEGRAM_API_KEY
 else
-  key=$2
+  key=$3
 fi
 
-nohup ngrok http $port &
+nohup ngrok http $port > ngrok.tmp &
 
-for i in `seq 1 5`; do
-  uri=`curl -s localhost:4040/api/tunnels | jq -r '.tunnels[] | select(.proto=="https") | .public_url'`
+for i in $(seq 1 5); do
+  uri=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[] | select(.proto=="https") | .public_url')
   if [ -z $uri ]; then
     echo "ngrok is not ready yet..."
     sleep 0.5  
   else
-    echo "ngrok url is $uri"
     break
   fi
 done 
 
 echo "the ngrok url is $uri"
 
-curl "https://api.telegram.org/bot$key/setWebhook?url=$uri/connectors/telegram"
+curl "https://api.telegram.org/bot$key/setWebhook?url=$uri$endpoint"
