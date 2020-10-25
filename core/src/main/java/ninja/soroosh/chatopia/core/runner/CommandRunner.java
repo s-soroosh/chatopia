def3a6117b04bbab36ffe011ac7eb35f0cf2bf73
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2020-10-25 22:49:56 +0100

    add runEvent

--- core/src/main/java/ninja/soroosh/chatopia/core/runner/CommandRunner.java
+++ core/src/main/java/ninja/soroosh/chatopia/core/runner/CommandRunner.java
@@ -4,4 +4,6 @@ import ninja.soroosh.chatopia.core.runner.responses.Response;
 
 public interface CommandRunner {
     Response run(Command command, Context context);
+
+    Response runEvent(Event<?> event, Context context);
 }
--- core/src/main/java/ninja/soroosh/chatopia/core/runner/SimpleCommandRunner.java
+++ core/src/main/java/ninja/soroosh/chatopia/core/runner/SimpleCommandRunner.java
@@ -1,9 +1,9 @@
 package ninja.soroosh.chatopia.core.runner;
 
 import lombok.extern.slf4j.Slf4j;
-import ninja.soroosh.chatopia.core.Rule;
+import ninja.soroosh.chatopia.core.CommandRule;
+import ninja.soroosh.chatopia.core.EventRule;
 import ninja.soroosh.chatopia.core.runner.responses.Response;
-import ninja.soroosh.chatopia.core.runner.responses.TextResponse;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.InvocationTargetException;
@@ -14,27 +14,29 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 public class SimpleCommandRunner implements CommandRunner {
-    private final List<Rule> rules;
+    private final List<CommandRule> commandRules;
+    private final List<EventRule> eventRules;
     private final SimpleContextEnricher contextEnricher;
     private static Response defaultResponse = Response.asText("Unknown command! \nPlease use command \"help\" to find out what capabilities I have!");
     private final Response helpResponse;
 
-    public SimpleCommandRunner(List<Rule> rules, SimpleContextEnricher contextEnricher) {
-        this.rules = rules;
+    public SimpleCommandRunner(List<CommandRule> commandRules, List<EventRule> eventRules, SimpleContextEnricher contextEnricher) {
+        this.commandRules = commandRules;
+        this.eventRules = eventRules;
         this.contextEnricher = contextEnricher;
         this.helpResponse = Response.asText(
-                rules.stream()
-                        .map(rule -> rule.getCommandName() + " - " + rule.getHelp())
+                commandRules.stream()
+                        .map(commandRule -> commandRule.getCommandName() + " - " + commandRule.getHelp())
                         .collect(Collectors.joining("\n"))
         );
     }
 
     @Override
     public Response run(final Command command, final Context context) {
-        final Optional<Rule> maybeMatchedRule = this.rules.stream()
-                .filter(rule -> command.name().equals(rule.getCommandName())
-                        || (rule.getCommandName().endsWith(" *")
-                        && command.name().startsWith(rule.getCommandName().substring(0, rule.getCommandName().lastIndexOf("\s")))))
+        final Optional<CommandRule> maybeMatchedRule = this.commandRules.stream()
+                .filter(commandRule -> command.name().equals(commandRule.getCommandName())
+                        || (commandRule.getCommandName().endsWith(" *")
+                        && command.name().startsWith(commandRule.getCommandName().substring(0, commandRule.getCommandName().lastIndexOf("\s")))))
                 .findFirst();
         if (maybeMatchedRule.isEmpty()) {
             if (command.name().equals("help")) {
@@ -44,18 +46,44 @@ public class SimpleCommandRunner implements CommandRunner {
         }
 
         final Context enrichedContext = contextEnricher.enrich(context);
-        final Rule matchedRule = maybeMatchedRule.get();
+        final CommandRule matchedCommandRule = maybeMatchedRule.get();
 
-        return runCommand(command, enrichedContext, matchedRule);
+        return runCommand(command, enrichedContext, matchedCommandRule);
     }
 
-    private Response runCommand(Command command, Context enrichedContext, Rule matchedRule) {
+    @Override
+    public Response runEvent(final Event<?> event, final Context context) {
+        final Optional<EventRule> maybeMatchedRule = this.eventRules.stream()
+                .filter(eventRule -> eventRule.getEventName().equals(event.getEventName()))
+                .findFirst();
+        if (maybeMatchedRule.isEmpty()) {
+            log.warn("unhandled event {}", event);
+            // TODO: EMPTY Response
+            return defaultResponse;
+        }
+
+        final Context enrichedContext = contextEnricher.enrich(context);
+        final EventRule matchedEventRule = maybeMatchedRule.get();
+
+        return runEvent(event, enrichedContext, matchedEventRule);
+    }
+
+    private Response runCommand(Command command, Context enrichedContext, CommandRule matchedCommandRule) {
         try {
-            return (Response) matchedRule.getMethod().invoke(
-                    matchedRule.getObject(),
+            return (Response) matchedCommandRule.getMethod().invoke(
+                    matchedCommandRule.getObject(),
                     command.name(),
                     enrichedContext
             );
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            log.error("Error In running command", e);
+            return defaultResponse;
+        }
+    }
+
+    public Response runEvent(Event<?> event, Context enrichedContext, EventRule eventRule) {
+        try {
+            return (Response) eventRule.getMethod().invoke(eventRule.getObject(), event, enrichedContext);
         } catch (IllegalAccessException | InvocationTargetException e) {
             log.error("Error In running command", e);
             return defaultResponse;
